ba855f9e39744c1892546c54b7a77bc887bbe4ed
Henry Okoli <henryokoli8@gmail.com>
2024-02-06 18:38:01 +0000

    i continue to update the file

--- scripts/main2.js
+++ scripts/main2.js
@@ -10,18 +10,35 @@ let data = await d3.csv("data/movies_mock.csv", (d) => {
         ratings_C: +d.ratings_C,
         release_month: +d.release_month,
         revenues: +d.revenues
-
-    // year: new Date(+d.Year, 0, 1), // convert "Year" column to Date
-    // make: d.Make,
-    // model: d.Model,
-    // length: +d.Length, // convert "Length" column to number
-    // registered: d.Reg==='Yes', // convert "Reg" column to boolean
-    // large: +d.Length > 100 // create new boolean attribute
-
 };
 });
 console.log(data);
 
+let data1 = [{name:'Pete',age:26},{name:'Johanna',age:27},
+            {name:'Jamie',age:38},{name:'Aleksy',age:29},
+            {name:'Akira',age:25},{name:'Adesina',age:32},
+            {name:'Lorenzo',age:30},{name:'Julien',age:26}];
 
-let data1 = await d3.csv("data/movies_mock.csv");
 console.log(data1);
+
+let sizes = data1.filter(d=>d.age<35 && d.age>25) // filter ages
+    .map(d=>d.name) // grab names only
+    .filter(d=>d.startsWith('J')) // filter first letters
+    .map(d=>d.length); // map to name length
+
+let avgSize = sizes.reduce((a,b)=>a+b, 0)/sizes.length; // sum sizes divided by length
+
+console.log(sizes);
+console.log(avgSize);
+
+let unique = new Set(data.map(d=>d.genre));
+console.log("unique genre", unique)
+
+let uniqueDirector = new Set(data.map(d=>d.director));
+let NumberOfuniqueDirector = uniqueDirector.size;
+console.log("unique Directors", uniqueDirector)
+console.log("Number of Nuique Directors", NumberOfuniqueDirector)
+
+let MoviesRelease = data.filter(d=>d.release_year<=2012 && d.release_year>=2014)
+    .map(d=>d.genre)
+console.log(MoviesRelease)
