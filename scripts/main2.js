95a9f5f4b3e73d7e50802ae8903cedf5a1a768a4
Henry Okoli <henryokoli8@gmail.com>
2024-02-27 00:02:07 +0000

    i continue to update the file

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,18 +1,21 @@
 let data = await d3.csv("data/movies_mock.csv", (d) => {
+
     // for each row d, return a new object
     return {
         release_year: +d.release_year,
-        budget: +d.budget,
+        budget: parseFloat(d.budget),
         director: d.director,
         genre: d.genre,
         ratings_A: +d.ratings_A,
         ratings_B: +d.ratings_B,
         ratings_C: +d.ratings_C,
         release_month: +d.release_month,
-        revenues: +d.revenues
+        revenues: parseFloat(d.revenues),
+        profits: parseFloat(d.revenues) - parseFloat(d.budget),
+        commercial_success: parseFloat(d.revenues) - parseFloat(d.budget) > 0
 };
 });
-console.log(data);
+console.log('Full data', data);
 
 let data1 = [{name:'Pete',age:26},{name:'Johanna',age:27},
             {name:'Jamie',age:38},{name:'Aleksy',age:29},
@@ -31,14 +34,20 @@ let avgSize = sizes.reduce((a,b)=>a+b, 0)/sizes.length; // sum sizes divided by 
 console.log(sizes);
 console.log(avgSize);
 
+// to calculate how many genres are in the movie
 let unique = new Set(data.map(d=>d.genre));
 console.log("unique genre", unique)
 
-let uniqueDirector = new Set(data.map(d=>d.director));
-let NumberOfuniqueDirector = uniqueDirector.size;
+// to calculate number of directors are in the movie
+let uniqueDirector = new Set(data.map(d=>d.director)).size;
 console.log("unique Directors", uniqueDirector)
-console.log("Number of Nuique Directors", NumberOfuniqueDirector)
 
-let MoviesRelease = data.filter(d=>d.release_year<=2012 && d.release_year>=2014)
-    .map(d=>d.genre)
+// calculate the total sum of the revenue generated
+let sum = (a,b)=>a+b;
+
+// movie release between 2012 and 2014
+let MoviesRelease = data.filter(d=>d.release_year>=2012 && d.release_year<=2014)
+    .map(d=>d.genre);
+let moveiSize = MoviesRelease.size;
 console.log(MoviesRelease)
+console.log(moveiSize)
